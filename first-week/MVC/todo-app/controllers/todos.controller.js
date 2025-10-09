7e56946a3379332d1ce8d9b448bbe5377418346e
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-10-09 15:38:36 +0530

    completed basic todoi

--- first-week/MVC/todo-app/controllers/todos.controller.js
+++ first-week/MVC/todo-app/controllers/todos.controller.js
@@ -1,12 +1,14 @@
-const { getData , addOrUpdateTodos } = require('../models/todos.model');
+const { strictEqual } = require('assert');
+const { getData, addOrUpdateTodos } = require('../models/todos.model');
+const todosRouter = require('../routes/todos.routes');
 
-const getAllTodos = (req , res) => {
-    let { todos , data } = getData();
-    res.json({ "All todos": todos  });
+const getAllTodos = (req, res) => {
+    let { todos, data } = getData();
+    res.json({ "All todos": todos });
 };
 
-const addTodo = (req , res) => {
-    let { todos , data } = getData();
+const addTodo = (req, res) => {
+    let { todos, data } = getData();
     let newTodo = req.body;
     const lastTodo = todos[todos.length - 1];
     const newId = lastTodo ? lastTodo.id + 1 : 1;
@@ -15,7 +17,60 @@ const addTodo = (req , res) => {
     data.todos = todos;
     addOrUpdateTodos(data);
     res.status(201).json({ "message": "Todo added successfully", "todo": newTodo });
-}
+};
 
-module.exports = { getAllTodos , addTodo }
+const updateTodo = (req, res) => {
+    let { todos, data } = getData();
+    const todoId = req.params.id;
+
+    const todoIndex = todos.findIndex((todo) => todo.id == todoId);
+
+    if (todoIndex === -1) {
+
+        res.status(404).json({ "message": "Todo not found" });
+    } else {
+        let updatedTodos = todos.map((todo) => {
+
+            if (todo.id == todoId) {
+                return { ...todo, ...req.body };
+            }
+            return todo;
+        });
+        data.todos = updatedTodos;
+        
+    }
+    addOrUpdateTodos(data);
+    res.status(200).json({ "message": "Todo updated successfully" , todos : todos});
+};
+
+const deleteTodo = (req, res) => {
+    let { todos, data } = getData();
+    const todoId = req.params.id;
+    const todoIndex = todos.findIndex((todo) => todo.id == todoId);
+    if (todoIndex === -1) {
+        res.status(404).json({ "message": "Todo not found" });
+    } else {
+        let updatedTodos = todos.filter((todo) => todo.id != todoId);
+        data.todos = updatedTodos;
+    }
+    addOrUpdateTodos(data);
+    res.status(200).json({ "message": "Todo deleted successfully" , todos : todos});
+};
+
+const searchTodo = (req, res) => {
+
+    let { todos , data } = getData();
+    let  title  = req.query.title.toLowerCase();
+
+    let filteredTodos = todos.filter((todo) => todo.title.toLowerCase().includes(title));
+
+    if(filteredTodos.length == 0){
+        res.status(404).json({ "message": "Todo not found" });
+    }else{
+    res.status(200).json({ "message": "Todo found successfully" , todos : filteredTodos});
+    }
+
+};
+
+module.exports = { getAllTodos, addTodo, updateTodo, deleteTodo, searchTodo };
 
--- first-week/MVC/todo-app/routes/todos.routes.js
+++ first-week/MVC/todo-app/routes/todos.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const todosRouter = express.Router();
 const fs = require('node:fs');
-const { getAllTodos , addTodo } = require('../controllers/todos.controller');
+const { getAllTodos, addTodo, updateTodo, deleteTodo, searchTodo } = require('../controllers/todos.controller');
 
 todosRouter.get('/all-todos', getAllTodos);
 todosRouter.post('/add-todo', addTodo);
+todosRouter.put('/update-todo/:id', updateTodo);
+todosRouter.delete('/delete-todo/:id', deleteTodo);
+todosRouter.get('/search-todo', searchTodo);
 
 module.exports = todosRouter;
