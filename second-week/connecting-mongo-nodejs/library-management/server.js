956c840b672b54cfb79367700bb9a44a47afdb9a
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-10-13 22:29:13 +0530

    layed foundation for library management

---
+++ second-week/connecting-mongo-nodejs/library-management/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3030;
+
+// app.use(express.json());
+
+app.get('/test', (req, res) => {
+    res.send('Test route is working')
+});
+
+app.use((req, res) => {
+    res.status(404).json({ 'error': '404 : Route Not found' })
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}`)
+})
+
