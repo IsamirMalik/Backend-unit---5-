19aaf7181bcaa680f2ec61db0a68f841c1ac5c9d
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-11-09 23:21:28 +0530

    finally added required things to the server.js , register our first user successfully

--- project/alertify/backend/server.js
+++ project/alertify/backend/server.js
@@ -1,23 +1,29 @@
 const express = require('express');
+const authRouter = require('./routes/auth.routes');
 const cors = require('cors');
+const { url } = require('inspector');
 const app = express();
 require('dotenv').config();
+const connectToDb = require('./configs/db');
+connectToDb();
 
-const PORT = process.env.PORT || 3030;
+const PORT =  8080;
 
-
+// app.use(urlencoded({ extended: true }));
 
 app.use(cors());
 
 app.use(express.json());
 // Test route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/test', (req, res) => {
+  res.json({ "message": "Test route is working" });
 });
+
+app.use('/api/auth', authRouter)
 
 // Handle undefined routes
 app.use((req, res) => {
-  res.status(404).send('404 Not Found');
+  res.status(404).json({ 'error': '404 : Route Not found' });
 });
 
 // Start the server
