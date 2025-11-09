11c079b37ca208a0e7b228f0b77a06b187196ec9
IsamirMalik <153701541+IsamirMalik@users.noreply.github.com>
2025-11-09 23:18:44 +0530

    added databawe url and defined the function to connect to db

---
+++ project/alertify/backend/configs/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/alertify');
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectToDb;
